1cccd70ec1f75a05b1655bc82544eecf3f28f784
lawtancool <26829131+lawtancool@users.noreply.github.com>
2020-06-09 10:05:22 -0700

    add docstrings to light.py

--- pyControl4/light.py
+++ pyControl4/light.py
@@ -10,9 +10,25 @@ class C4Light:
         self.item_id = item_id
 
     async def getLevel(self):
-        return await self.director.getItemVariableValue(self.item_id, "LIGHT_LEVEL")
+        """Returns the level of a dimming-capable light as an int 0-100.
+        Will cause an error if called on a non-dimmer switch. Use getState() instead.
+        """
+        value = await self.director.getItemVariableValue(self.item_id, "LIGHT_LEVEL")
+        return int(value)
+
+    async def getState(self):
+        """Returns the power state of a dimmer or switch as a boolean (True=on, False=off).
+        """
+        value = await self.director.getItemVariableValue(self.item_id, "LIGHT_STATE")
+        return bool(value)
 
     async def setLevel(self, level):
+        """Sets the light level of a dimmer or turns on/off a switch. 
+        Any level > 0 will turn on a switch, and level = 0 will turn off a switch.
+
+        Parameters:
+            level - (int) 0-100
+        """
         await self.director.sendPostRequest(
             "/api/v1/items/{}/commands".format(self.item_id),
             "SET_LEVEL",
@@ -20,6 +36,13 @@ class C4Light:
         )
 
     async def rampToLevel(self, level, time):
+        """Ramps the light level of a dimmer over time. 
+        Any level > 0 will turn on a switch, and level = 0 will turn off a switch.
+
+        Parameters:
+            level - (int) 0-100
+            time - (int) Duration in milliseconds
+        """
         await self.director.sendPostRequest(
             "/api/v1/items/{}/commands".format(self.item_id),
             "RAMP_TO_LEVEL",
