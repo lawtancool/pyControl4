0d39febcc0bb2376e846af2f703ed73c877b31cd
lawtancool <26829131+lawtancool@users.noreply.github.com>
2020-06-09 18:58:23 -0700

    update light.py docstrings

--- pyControl4/light.py
+++ pyControl4/light.py
@@ -1,3 +1,5 @@
+"""Control Control4 Light devices.
+"""
 import aiohttp
 import asyncio
 import async_timeout
@@ -6,12 +8,19 @@ from .director import C4Director
 
 class C4Light:
     def __init__(self, C4Director, item_id):
+        """Creates a Control4 Light object.
+
+        Parameters:
+            `C4Director` - A `C4Director` object that corresponds to the Control4 Director that the light is connected to. See `director.py` for details.
+
+            `item_id` - The Control4 item ID of the light.
+        """
         self.director = C4Director
         self.item_id = item_id
 
     async def getLevel(self):
         """Returns the level of a dimming-capable light as an int 0-100.
-        Will cause an error if called on a non-dimmer switch. Use getState() instead.
+        Will cause an error if called on a non-dimmer switch. Use `getState()` instead.
         """
         value = await self.director.getItemVariableValue(self.item_id, "LIGHT_LEVEL")
         return int(value)
@@ -24,10 +33,10 @@ class C4Light:
 
     async def setLevel(self, level):
         """Sets the light level of a dimmer or turns on/off a switch. 
-        Any level > 0 will turn on a switch, and level = 0 will turn off a switch.
+        Any `level > 0` will turn on a switch, and `level = 0` will turn off a switch.
 
         Parameters:
-            level - (int) 0-100
+            `level` - (int) 0-100
         """
         await self.director.sendPostRequest(
             "/api/v1/items/{}/commands".format(self.item_id),
@@ -37,11 +46,12 @@ class C4Light:
 
     async def rampToLevel(self, level, time):
         """Ramps the light level of a dimmer over time. 
-        Any level > 0 will turn on a switch, and level = 0 will turn off a switch.
+        Any `level > 0` will turn on a switch, and `level = 0` will turn off a switch.
 
         Parameters:
-            level - (int) 0-100
-            time - (int) Duration in milliseconds
+            `level` - (int) 0-100
+
+            `time` - (int) Duration in milliseconds
         """
         await self.director.sendPostRequest(
             "/api/v1/items/{}/commands".format(self.item_id),
