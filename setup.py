5a909cfbe730f9f4e526c8435ab646ceb75278be
lawtancool <26829131+lawtancool@users.noreply.github.com>
2020-06-27 17:39:32 -0700

    add setup.py for release to pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyControl4",  # Replace with your own username
+    version="0.0.1",
+    author="lawtancool",
+    author_email="26829131+lawtancool@users.noreply.github.com",
+    description="Python 3 asyncio package for interacting with Control4 systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lawtancool/pyControl4",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
