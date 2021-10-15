d7ced90debf52a04836bfb73d1df0915673a6054
lawtancool <26829131+lawtancool@users.noreply.github.com>
2021-10-15 12:09:27 -0700

    Update setup.py email

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyControl4",  # Replace with your own username
-    version="0.3.0",
+    version="0.3.1",
     author="lawtancool",
-    author_email="contact@lawrencetan.ml",
+    author_email="contact@lawrencetan.ca",
     description="Python 3 asyncio package for interacting with Control4 systems",
     long_description=long_description,
     long_description_content_type="text/markdown",
