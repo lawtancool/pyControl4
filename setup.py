243c0b0c53f755718104ece3e916b5c565639f1a
lawtancool <26829131+lawtancool@users.noreply.github.com>
2021-11-27 19:06:30 -0800

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyControl4",  # Replace with your own username
-    version="1.0.0b2",
+    version="1.0.0b3",
     author="lawtancool",
     author_email="contact@lawrencetan.ca",
     description="Python 3 asyncio package for interacting with Control4 systems",
@@ -19,4 +19,10 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    install_requires=[
+        'aiohttp',
+        'xmltodict',
+        'python-socketio>=4,<5',
+        'websocket-client',
+     ],
 )
