d3eb2dac3ce9ace1aeff79042a7ce57eb864e4b5
lawtancool <26829131+lawtancool@users.noreply.github.com>
2021-12-29 14:53:58 -0800

    Increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyControl4",  # Replace with your own username
-    version="1.0.0b4",
+    version="1.0.0b5",
     author="lawtancool",
     author_email="contact@lawrencetan.ca",
     description="Python 3 asyncio package for interacting with Control4 systems",
@@ -24,5 +24,5 @@ setuptools.setup(
         'xmltodict',
         'python-socketio>=4,<5',
         'websocket-client',
-     ],
+    ],
 )
