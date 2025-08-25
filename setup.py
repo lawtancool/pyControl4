6249abd2c19c85843093f14dc07f2673359dfdae
lawtancool <26829131+lawtancool@users.noreply.github.com>
2025-08-24 22:44:02 -0700

    Bump to 1.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyControl4",  # Replace with your own username
-    version="1.4.0",
+    version="1.5.0",
     author="lawtancool",
     author_email="contact@lawrencetan.ca",
     description="Python 3 asyncio package for interacting with Control4 systems",
